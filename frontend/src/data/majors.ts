a232985c8e43cbe21ee8a378dadc41da41d20ed1
betich <betich365@gmail.com>
2023-03-26 03:31:03 +0700

    feat: add majors section

---
+++ frontend/src/data/majors.ts
@@ -0,0 +1,118 @@
+export interface Major {
+  id: string;
+  nameTH: string;
+  nameEN: string;
+}
+
+export const ThaiMajors: Major[] = [
+  {
+    id: "civil",
+    nameTH: "วิศวกรรมโยธา",
+    nameEN: "Civil Engineering",
+  },
+  //วิศวกรรมไฟฟ้า (Electrical Engineering)
+  {
+    id: "electrical",
+    nameTH: "วิศวกรรมไฟฟ้า",
+    nameEN: "Electrical Engineering",
+  },
+  // วิศวกรรมเครื่องกล (Mechanical Engineering)
+  {
+    id: "mechanical",
+    nameTH: "วิศวกรรมเครื่องกล",
+    nameEN: "Mechanical Engineering",
+  },
+  // วิศวกรรมยานยนต์ (Automative Engineering)
+  {
+    id: "automative",
+    nameTH: "วิศวกรรมยานยนต์",
+    nameEN: "Automative Engineering",
+  },
+  // วิศวกรรมอุตสาหการ (Industrial Engineering)
+  {
+    id: "ie",
+    nameTH: "วิศวกรรมอุตสาหการ",
+    nameEN: "Industrial Engineering",
+  },
+  // วิศวกรรมสิ่งแวดล้อม (Environmental Engineering)
+  {
+    id: "environmental",
+    nameTH: "วิศวกรรมสิ่งแวดล้อม",
+    nameEN: "Environmental Engineering",
+  },
+  // วิศวกรรมโลหการ (Metallurgical Engineering)
+  {
+    id: "metal",
+    nameTH: "วิศวกรรมโลหการ",
+    nameEN: "Metallurgical Engineering",
+  },
+  // วิศวกรรมเหมืองแร่และปิโตรเลียม (Mining and Petroleum Engineering)
+  {
+    id: "mining",
+    nameTH: "วิศวกรรมเหมืองแร่และปิโตรเลียม",
+    nameEN: "Mining and Petroleum Engineering",
+  },
+  // วิศวกรรมสำรวจ (Survey Engineering)
+  {
+    id: "survey",
+    nameTH: "วิศวกรรมสำรวจ",
+    nameEN: "Survey Engineering",
+  },
+  // วิศวกรรมเคมี (Chemical Engineering)
+  {
+    id: "chemical",
+    nameTH: "วิศวกรรมเคมี",
+    nameEN: "Chemical Engineering",
+  },
+  // วิศวกรรมคอมพิวเตอร์ (Computer Engineering)
+  {
+    id: "computer",
+    nameTH: "วิศวกรรมคอมพิวเตอร์",
+    nameEN: "Computer Engineering",
+  },
+  // วิศวกรรมคอมพิวเตอร์และเทคโนโลยีดิจิทัล (CEDT)
+  {
+    id: "comp-cedt",
+    nameTH: "วิศวกรรมคอมพิวเตอร์และเทคโนโลยีดิจิทัล",
+    nameEN: "Computer and Digital Technology Engineering",
+  },
+];
+
+export const InterMajors: Major[] = [
+  // วิศวกรรมหุ่นยนต์และปัญญาประดิษฐ์ (Robotics & AI)
+  {
+    id: "ai",
+    nameTH: "วิศวกรรมหุ่นยนต์และปัญญาประดิษฐ์",
+    nameEN: "Robotics & AI",
+  },
+  // วิศวกรรมสารสนเทศและการสื่อสาร (ICE)
+  {
+    id: "ice",
+    nameTH: "วิศวกรรมสารสนเทศและการสื่อสาร",
+    nameEN: "ICE",
+  },
+  // วิศวกรรมนาโน (NANO)
+  {
+    id: "nano",
+    nameTH: "วิศวกรรมนาโน",
+    nameEN: "NANO",
+  },
+  // วิศวกรรมการออกแบบและการผลิตยานยนต์ (ADME)
+  {
+    id: "adme",
+    nameTH: "วิศวกรรมการออกแบบและการผลิตยานยนต์",
+    nameEN: "ADME",
+  },
+  // วิศวกรรมอากาศยาน (AERO)
+  {
+    id: "aero",
+    nameTH: "วิศวกรรมอากาศยาน",
+    nameEN: "AERO",
+  },
+  // Chemical and Process Engineering (ChPe)
+  {
+    id: "chpe",
+    nameTH: "Chemical and Process Engineering",
+    nameEN: "ChPe",
+  },
+];
